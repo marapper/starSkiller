1db60c98783a68bcd476e3e188e5c10ae9c78031
Sergey Bondar <Bondar.S.Evg@sberbank.ru>
2020-05-08 22:10:29 +0300

    chore: gh

---
+++ build/precache-manifest.57f5754ea33ccb4b3ba2d191a7573c9b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "274378703298fd23353d237b23e168d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bacf0d6275cae3419333",
+    "url": "/static/css/main.22a474cf.chunk.css"
+  },
+  {
+    "revision": "c7f8e3ee4c62facb03ac",
+    "url": "/static/js/2.9131ae38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9131ae38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bacf0d6275cae3419333",
+    "url": "/static/js/main.cdd11f23.chunk.js"
+  },
+  {
+    "revision": "490fb67cd59a4d014133",
+    "url": "/static/js/runtime-main.49bcd6a6.js"
+  },
+  {
+    "revision": "bddce92ea956995da867992794b51290",
+    "url": "/static/media/cork-board-background.bddce92e.jpg"
+  },
+  {
+    "revision": "95dfd5c1a115549dfa0beaba497de327",
+    "url": "/static/media/header-wood.95dfd5c1.jpg"
+  }
+]);
